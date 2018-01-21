ca032dc725b371f167c1dd7b2579644733bccb6d
whysafish108 <katherineritchie99@gmail.com>
2018-01-21 15:07:05 -0600

    basic inputs passing test cases

--- src/CamelCaser.java
+++ src/CamelCaser.java
@@ -1,6 +1,37 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class CamelCaser {
     public static String camelCase(String inputString){
-        System.out.println("Configuration functioning");
-        return inputString;
+        /*if (inputString.contains([^A-Z][^0-9]))) {
+
+            throw new IllegalArgumentException();
+            System.out.println(ErrorConstants.INVALID_CHARACTER);
+        }*/
+        if (inputString == null) {
+            System.out.println("Null Input");
+            throw new IllegalArgumentException();
+        }
+        String camelCasedOutput = "";
+        inputString = inputString.toLowerCase();
+        String[] inputStrArr = inputString.split("\\s+");
+        for (int i = 0; i < inputStrArr.length; i++) {
+            System.out.print(i + ": ");
+            System.out.println(inputStrArr[i]);
+        }
+        for (int i = 0; i < inputStrArr.length; i++) {
+            if (inputStrArr[i] == "")) {
+                if (!camelCasedOutput.equals("")) {
+                    String firstCharString = inputStrArr[i].substring(0,1);
+                    inputStrArr[i] = inputStrArr[i].replaceFirst(firstCharString, firstCharString.toUpperCase());
+                    /*if (firstCharString.charAt(0) == ([0-9])) {
+                        System.out.println(ErrorConstants.INVALID_FORMAT);
+                        throw new IllegalArgumentException();
+                    }*/
+                }
+                camelCasedOutput = camelCasedOutput + inputStrArr[i];
+            }
+        }
+        return camelCasedOutput;
     }
 }
