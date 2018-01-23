d2430c935021685fc736f37b263659fe5efd44b4
whysafish108 <katherineritchie99@gmail.com>
2018-01-22 18:41:02 -0600

    all test cases passing

--- src/CamelCaser.java
+++ src/CamelCaser.java
@@ -8,35 +8,42 @@ public class CamelCaser {
      * Camel Cases a valid input
      * @param inputString String
      * @return String camelCasedOutput
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException invalid input
      */
     public static String camelCase(String inputString) throws IllegalArgumentException {
-        if (!inputString.matches("[a-zA-Z0-9]+")) {
 
-            System.out.println(ErrorConstants.INVALID_CHARACTER);
-            throw new IllegalArgumentException();
-        }
+        //checks for null input
         if (inputString == null) {
             System.out.println("Null Input");
             throw new IllegalArgumentException();
         }
+
         String camelCasedOutput = "";
         inputString = inputString.toLowerCase();
         String[] inputStrArr = inputString.split("\\s+");
-        for (int i = 0; i < inputStrArr.length; i++) {
-            System.out.print(i + ": ");
-            System.out.println(inputStrArr[i]);
-        }
+
+        //assembles output
         for (int i = 0; i < inputStrArr.length; i++) {
             if (!inputStrArr[i].equals("")) {
-                if (!camelCasedOutput.equals("")) {
-                    String firstCharString = inputStrArr[i].substring(0,1);
-                    inputStrArr[i] = inputStrArr[i].replaceFirst(firstCharString, firstCharString.toUpperCase());
-                    if (firstCharString.matches("[0-9]")) {
-                        System.out.println(ErrorConstants.INVALID_FORMAT);
-                        throw new IllegalArgumentException();
-                    }
+
+                //checks if input is exclusively alphanumerical
+                if (!inputStrArr[i].matches("[a-zA-Z0-9]+")) {
+                    System.out.println(ErrorConstants.INVALID_CHARACTER);
+                    throw new IllegalArgumentException();
                 }
+
+                String firstCharString = inputStrArr[i].substring(0,1);
+                //checks that word does not start with number
+                if (firstCharString.matches("[0-9]")) {
+                    System.out.println(ErrorConstants.INVALID_FORMAT);
+                    throw new IllegalArgumentException();
+                }
+
+                //modifies second word to begin with a capital letter
+                if (!camelCasedOutput.equals("")) {
+                    inputStrArr[i] = inputStrArr[i].replaceFirst(firstCharString, firstCharString.toUpperCase());
+                }
+
                 camelCasedOutput = camelCasedOutput + inputStrArr[i];
             }
         }
